b7246a878ddfbdad95687c70e14d7adbf307c647
GRPeres <gr.peres@hotmail.com>
2026-07-21 23:08:29 -0300

    Lock installed PWA to portrait orientation

--- src/MangaHub.Web/wwwroot/service-worker.js
+++ src/MangaHub.Web/wwwroot/service-worker.js
@@ -1,11 +1,11 @@
-﻿const CACHE_NAME = "mangahub-app-v29";
+﻿const CACHE_NAME = "mangahub-app-v30";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
   "/icons/book.svg",
-  "/css/app.css?v=40",
-  "/MangaHub.Web.styles.css?v=40"
+  "/css/app.css?v=41",
+  "/MangaHub.Web.styles.css?v=41"
 ];
 
 self.addEventListener("install", event => {
