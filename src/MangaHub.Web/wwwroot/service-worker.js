0b8b93bb540353a78fe62c93761a652e46699f93
GRPeres <GRPeres@users.noreply.github.com>
2026-09-20 21:46:28 -0300

    Make reader language menu scrollable

--- src/MangaHub.Web/wwwroot/service-worker.js
+++ src/MangaHub.Web/wwwroot/service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "mangahub-app-v222";
+const CACHE_NAME = "mangahub-app-v223";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/offline.html",
   "/manifest.webmanifest",
   "/icons/book.svg",
-  "/css/app.css?v=233",
+  "/css/app.css?v=234",
   "/MangaHub.Web.styles.css?v=223"
 ];
 
