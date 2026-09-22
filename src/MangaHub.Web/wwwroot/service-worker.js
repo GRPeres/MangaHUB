cc469cb80db32c545d45b601c8a41f021e7cf972
GRPeres <GRPeres@users.noreply.github.com>
2026-09-22 09:21:56 -0300

    Top align expanded mobile synopsis

--- src/MangaHub.Web/wwwroot/service-worker.js
+++ src/MangaHub.Web/wwwroot/service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "mangahub-app-v225";
+const CACHE_NAME = "mangahub-app-v226";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/offline.html",
   "/manifest.webmanifest",
   "/icons/book.svg",
-  "/css/app.css?v=236",
+  "/css/app.css?v=237",
   "/MangaHub.Web.styles.css?v=223"
 ];
 
